fbeb53cf98bb7ff48b0dc2434ef597e3b77353d6
Brett Boston <bboston7@gmail.com>
2013-10-10 00:08:22 -0700

    Added basic connection file

---
+++ src/connection.rkt
@@ -0,0 +1,10 @@
+#!/usr/bin/racket
+#lang racket
+
+; TODO: Move these into a config file
+(define HOST "")
+(define PORT 6667)
+(define NICK "")
+(define IDENT "")
+(define REALNAME "")
+(define CHAN "")
