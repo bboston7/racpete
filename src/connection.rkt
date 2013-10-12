5607a9543ab599ac42d1271eabb995a2cfd5d039
Brett Boston <bboston7@gmail.com>
2013-10-11 22:47:52 -0700

    Can now connect to IRC servers that don't require ident
    irc.freenode.net and irc.adelais.net both don't require ident, so I don't
    really think we need to worry about it.  Quakenet and some others do, but for
    now we should just live without it

--- src/connection.rkt
+++ src/connection.rkt
@@ -2,9 +2,38 @@
 #lang racket
 
 ; TODO: Move these into a config file
-(define HOST "")
+(define HOST "irc.freenode.net")
 (define PORT 6667)
-(define NICK "")
-(define IDENT "")
-(define REALNAME "")
+(define NICK "racpete")
+(define IDENT "racpete")
+(define REALNAME "Rac Pete")
 (define CHAN "")
+
+#|
+Sets input to the input stream from the server and output to the output stream
+from our computer
+|#
+(define-values (input output) (tcp-connect HOST PORT))
+
+#|
+Identifies with the IRC Server
+|#
+(define (identify)
+  (begin
+    (write-string (string-append "NICK " NICK "\r\n") output)
+    (write-string (string-append "USER " IDENT " 0 * :" REALNAME "\r\n") output)
+    (flush-output output)))
+
+#|
+Prints out data returned from the server
+|#
+(define (read-in)
+  (begin
+    (display (read-line input))
+    (read-in)))
+
+(identify)
+(read-in)
+
+(close-output-port output)
+(close-input-port input)
