d188970aef8243be34f1e28bd13fc76363b99637
Brett Boston <bboston7@gmail.com>
2013-12-16 00:28:58 -0800

    Added types to string-utils

--- src/util/string-utils.rkt
+++ src/util/string-utils.rkt
@@ -1,19 +1,20 @@
-#lang racket
+#lang typed/racket
 
-(provide (contract-out
-           [strip-tags (-> string? string?)]
-           [string-contains? (-> string? string? boolean?)]
-           [string-starts-with? (-> string? string? boolean?)]))
+(provide strip-tags
+         string-contains?
+         string-starts-with?)
 
 #|
 Strips tags and the string "\n" out of str
 |#
+(: strip-tags (String -> String))
 (define (strip-tags str)
   (regexp-replace* #rx"<.*?>|\\\n" str ""))
 
 #|
 Returns a true value if token is in str
 |#
+(: string-contains? (String String -> Boolean))
 (define (string-contains? str token)
   (regexp-match?
     (regexp (regexp-quote (string-downcase token)))
@@ -22,6 +23,7 @@ Returns a true value if token is in str
 #|
 Returns true if str starts with token
 |#
+(: string-starts-with? (String String -> Boolean))
 (define (string-starts-with? str token)
   (regexp-match?
     (regexp (string-append "^" (regexp-quote (string-downcase token))))
