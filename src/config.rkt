d7815f649752502c87794c25b7fe2fbf54cf66c5
Max Sherman <john@maxsherman.com>
2014-03-10 23:14:06 -0700

    reverse commit to config

--- src/config.rkt
+++ src/config.rkt
@@ -2,12 +2,12 @@
 
 (provide (all-defined-out))
 
-(define: HOST : String "irc.adelais.net")
+(define: HOST : String "example.server.com")
 (define: PORT : Positive-Integer 6667)
 (define: NICK : String "racpete")
 (define: IDENT : String "racpete")
 (define: REALNAME : String "racpete")
-(define: CHAN : String "#poopers")
+(define: CHAN : String "#examplechannel")
 (define RECONNECT_TIMER 30)
 
 (define: GOOGLE_API_KEY : String "")
