02db6fa4d53b2839ffbe5e84a123f37fb9f9b146
Max Sherman <john@maxsherman.com>
2014-06-19 22:53:28 -0700

    [derek] added let rec.  this makes derek turing complete!

--- src/util/derek.rkt
+++ src/util/derek.rkt
@@ -8,14 +8,14 @@
 
 #|
 
-This is derek, a language of mathematical expressions that pete recognizes and
-can compute.
+This is derek, an ML-like language that pete recognizes and can evaluate
 
 |#
 
 (define TIMEOUT 2000)
 (define MAX-LEN 400)
 
+;; exp nodes
 (struct num-node (n))
 (struct minus-num-node (n))
 (struct plus-node (l r))
@@ -29,11 +29,23 @@ can compute.
 (struct bool-node (b))
 (struct lt-node (l r))
 (struct let-in-node (i e1 e2))
+(struct let-rec-in-node (f x t e1 e2))
 (struct var-node (i))
+(struct lambda-node (x t e))
+(struct app-node (e1 e2))
+(struct exp-node (e))
+
+;; type nodes, need transparency for equal? to work
+(struct numt-node () #:transparent)
+(struct boolt-node () #:transparent)
+(struct arrowt-node (l r) #:transparent)
+
+;; need mutability to implement let rec
+(struct closure (e env arg) #:mutable)
 
 (define-tokens ts (NUM BOOL VAR))
-(define-empty-tokens ets (+ - * / % ^ < = LPAREN RPAREN IF THEN ELSE LET IN
-                            LETEND END FAIL))
+(define-empty-tokens ets (+ - * / % ^ < = : LPAREN RPAREN IF THEN ELSE LET IN
+                            LETEND LAMBDA ARROW BOOLT NUMT DOT APP REC END FAIL))
 
 ;; Scanner: returns #f on unmatchable input
 (define scanner
@@ -47,6 +59,7 @@ can compute.
          [")" (token-RPAREN)]
          ["<" (token-<)]
          ["=" (token-=)]
+         ["." (token-DOT)]
          [(union
             (concatenation (repetition 1 +inf.0 numeric)
                            (repetition 0 1 ".")
@@ -62,6 +75,12 @@ can compute.
          ["let" (token-LET)]
          ["in" (token-IN)]
          ["end" (token-LETEND)]
+         ["\\" (token-LAMBDA)]
+         [":" (token-:)]
+         ["->" (token-ARROW)]
+         ["bool" (token-BOOLT)]
+         ["num" (token-NUMT)]
+         ["rec" (token-REC)]
          [(repetition 1 +inf.0 alphabetic) (token-VAR lexeme)]
          [whitespace (scanner input-port)]
          [(eof) (token-END)]
@@ -71,9 +90,8 @@ can compute.
 (define parse
   (parser
     (grammar (E
-               ((NUM) (num-node (string->number $1)))
-               ((BOOL) (bool-node $1))
-               ((VAR) (var-node $1))
+               ((IF E THEN E ELSE E) (prec IF) (if-node $2 $4 $6))
+               ((LAMBDA VAR : TYPE DOT E) (prec LAMBDA) (lambda-node $2 $4 $6))
                ((- E) (prec +) (minus-num-node $2))
                ((E + E) (prec +) (plus-node $1 $3))
                ((E - E) (prec -) (minus-node $1 $3))
@@ -82,53 +100,93 @@ can compute.
                ((E / E) (prec /) (divide-node $1 $3))
                ((E ^ E) (prec ^) (power-node $1 $3))
                ((E < E) (prec <) (lt-node $1 $3))
+               ((A) (exp-node $1))
+               )
+             (A
+               ((F A) (app-node $1 $2))
+               ((F) (exp-node $1))
+               )
+             (F
+               ((NUM) (num-node (string->number $1)))
+               ((BOOL) (bool-node $1))
+               ((VAR) (var-node $1))
                ((LPAREN E RPAREN) (prec LPAREN) (paren-node $2))
-               ((IF E THEN E ELSE E) (prec IF) (if-node $2 $4 $6))
                ((LET VAR = E IN E LETEND) (prec LET) (let-in-node $2 $4 $6))
-               ))
+               ((LET REC VAR VAR : TYPE = E IN E LETEND)
+                (prec LET) (let-rec-in-node $3 $4 $6 $8 $10))
+               )
+             (TYPE
+               ((BOOLT) (boolt-node))
+               ((NUMT) (numt-node))
+               ((TYPE ARROW TYPE) (prec ARROW) (arrowt-node $1 $3))
+               ((LPAREN TYPE RPAREN) (prec LPAREN) (paren-node $2))
+               )
+             )
     (tokens ts ets)
     (start E)
     (end END)
     (error (lambda (a b c) #f))
-    (precs (nonassoc IF THEN ELSE)
+    (precs (left APP)
+           (nonassoc IF)
            (nonassoc LET)
+           (nonassoc LAMBDA)
+           (right ARROW)
            (nonassoc <)
            (left + -)
            (left * / %)
            (right ^)
-           (nonassoc LPAREN RPAREN))))
+           (nonassoc LPAREN RPAREN)
+           )))
 
 ;; typechecker
 (define (tc ast env)
-  (let ([num? (lambda (x) (eq? x 'num))]
-        [bool? (lambda (x) (eq? x 'bool))])
+  (let ([num (numt-node)]
+        [num? numt-node?]
+        [bool (boolt-node)]
+        [bool? boolt-node?]
+        [arrow? arrowt-node?])
     (match ast
-           [(struct num-node (_)) 'num]
-           [(struct bool-node (_)) 'bool]
-           [(struct plus-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct minus-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct times-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct divide-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct mod-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct power-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'num)]
-           [(struct minus-num-node (n)) (and (num? (tc n env)) 'num)]
+           [(struct num-node (_)) num]
+           [(struct bool-node (_)) bool]
+           [(struct plus-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct minus-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct times-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct divide-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct mod-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct power-node (l r)) (and (num? (tc l env)) (num? (tc r env)) num)]
+           [(struct minus-num-node (n)) (and (num? (tc n env)) num)]
            [(struct paren-node (e)) (tc e env)]
-           [(struct lt-node (l r)) (and (num? (tc l env)) (num? (tc r env)) 'bool)]
+           [(struct lt-node (l r)) (and (num? (tc l env)) (num? (tc r env)) bool)]
            [(struct if-node (t e1 e2)) (let ([tt (tc t env)]
                                              [e1t (tc e1 env)]
                                              [e2t (tc e2 env)])
-                                         (and (bool? tt) (eq? e1t e2t) e1t))]
+                                         (and (bool? tt) (equal? e1t e2t) e1t))]
            [(struct let-in-node (i e1 e2)) (tc e2 (cons `(,i . ,(tc e1 env)) env))]
            [(struct var-node (i)) (let ([t (assoc i env)]) (and t (cdr t)))]
+           [(struct lambda-node (x t e)) (let ([rt (tc e (cons `(,x . ,t) env))])
+                                           (and rt (arrowt-node t rt)))]
+           [(struct app-node (e1 e2)) (let ([t2 (tc e2 env)]
+                                            [t1 (tc e1 env)])
+                                        (and (arrow? t1)
+                                             (equal? (arrowt-node-l t1) t2)
+                                             (arrowt-node-r t1)))]
+           [(struct exp-node (e)) (tc e env)]
+           [(struct let-rec-in-node (f x t e1 e2))
+            (let ([rt (and (arrow? t)
+                           (tc e1 (cons `(,x . ,(arrowt-node-l t)) (cons `(,f . ,t) env))))])
+              (and rt (tc e2 (cons `(,f . ,t) env))))]
            [_ (error "unrecognized case in tc")]
            )))
 
 ;; holds the result of an evaluation
 (struct res (r))
+
+;; res struct to string
 (define (r->s r)
   (let ([v (res-r r)])
     (cond [(boolean? v) (if v "true" "false")]
           [(number? v) (number->string v)]
+          [(closure? v) "<fun>"]
           [else (error "unrecognized type")])))
 
 ;; Interpreter: returns a res struct containing the result of evaluating the
@@ -138,6 +196,7 @@ can compute.
     (match e
            [(struct num-node (n)) n]
            [(struct bool-node (b)) b]
+           [(struct lambda-node (x t e)) (closure e env x)]
            [(struct var-node (i)) (cdr (assoc i env))]
            [(struct minus-num-node (n)) (- (eval* n env))]
            [(struct plus-node (l r)) (+ (eval* l env) (eval* r env))]
@@ -150,6 +209,15 @@ can compute.
            [(struct lt-node (l r)) (< (eval* l env) (eval* r env))]
            [(struct if-node (t e1 e2)) (if (eval* t env) (eval* e1 env) (eval* e2 env))]
            [(struct let-in-node (i e1 e2)) (eval* e2 (cons `(,i . ,(eval* e1 env)) env))]
+           [(struct app-node (e1 e2)) (let ([arg (eval* e2 env)]
+                                            [cl (eval* e1 env)])
+                                        (eval* (closure-e cl)
+                                               (cons `(,(closure-arg cl) . ,arg) (closure-env cl))))]
+           [(struct exp-node (e)) (eval* e env)]
+           [(struct let-rec-in-node (f x t e1 e2))
+            (let ([fn (closure e1 (void) x)])
+              (set-closure-env! fn (cons `(,f . ,fn) env)) ; ugh
+              (eval* e2 (cons `(,f . ,fn) env)))]
            [_ (error "unrecognized case in eval")]
            ))
   (res (eval* e env)))
@@ -290,4 +358,41 @@ can compute.
                        (number->string (let ([y 5]) (let ([y 4]) (expt y 2)))))
          (check-equal? (try-eval "let maxiscool = 5 in maxiscool+1 end")
                        (number->string (let ([maxiscool 5]) (+ maxiscool 1))))
+
+         ;; function tests
+         (check-equal? (try-eval "\\ x : num . x + 100") "<fun>")
+         (check-eq? (try-eval "let x = \\ x : bool . x + 100 in x end") #f)
+         (check-equal? (try-eval "let x = \\ x : num . if true then x + 100
+                                 else 4 in x end") "<fun>")
+         (check-equal? (try-eval "\\ x : (num -> num -> num) . x") "<fun>")
+         (check-equal? (try-eval "(\\ x : num . x + 1) 5")
+                       (number->string (add1 5)))
+         (check-eq? (try-eval "\\ x : num . x + 1 5") #f)
+         (check-equal? (try-eval "let f = \\ x : num . x + 4 in f 5 end")
+                       (number->string (let ([f (lambda (x) (+ x 4))]) (f 5))))
+         (check-equal? (try-eval "let f = \\ y :bool . if y then 1 else 2 in f true end")
+                       (number->string (let ([f (lambda (y) (if y 1 2))]) (f #t))))
+         (check-equal? (try-eval "(if 3 < 5 then \\ x : num . x else \\ y : num . y + 1) 5")
+                       (number->string ((if (< 3 5) identity add1) 5)))
+         (check-eq? (try-eval "(if 3 < 5 then \\ x : bool . x else \\ y : num .
+                                 y + 1) 5") #f)
+         (check-eq? (try-eval "(if 3 < 5 then \\ x : num . true else \\ y : num .
+                                 y + 1) 5") #f)
+         (check-eq? (try-eval "(if 3 < 5 then \\ x : num . x else \\ y : num .
+                                 x + 1) 5") #f)
+         (check-equal? (try-eval "((\\ f : num -> num . \\ x : num . f x)
+                                  (\\ x : num . x + 1)) 6")
+                       (number->string (apply add1 '(6))))
+         (check-equal? (try-eval "let rec fact n : num->num = if n < 2 then 1 else n * fact (n - 1) in fact 5 end") "120")
+         (check-eq? (try-eval "let rec fact n : num->bool = if n < 2 then 1 else n * fact (n - 1) in fact 5 end") #f)
+         (check-equal? (try-eval "let rec isprime n : num->bool =
+                                 let rec aux m : num->bool =
+                                 if m < 2 then true else if n % m < 1
+                                 then false else aux (m - 1) in aux (n - 1) end
+                                 in isprime 99 end") "false")
+         (check-equal? (try-eval "let rec isprime n : num->bool =
+                                 let rec aux m : num->bool =
+                                 if m < 2 then true else if n % m < 1
+                                 then false else aux (m - 1) in aux (n - 1) end
+                                 in isprime 83 end") "true")
          )
