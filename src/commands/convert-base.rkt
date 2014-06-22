ed733c85b6c51592b0c48ac10ecea7578967c5f5
Ben Eggers <beggerss@cs.washington.edu>
2014-06-22 02:35:19 -0700

    initial base-converting

---
+++ src/commands/convert-base.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(provide
+	convert-base
+)
+
+#|
+Convert num from base from-rad to base to-rad
+|#
+(define (convert-base num from-rad to-rad)
+	(cond
+		[(not (andmap string? (list num from-rad to-rad))) #f]
+		[else (convert-to-string (string->number num (string->number from-rad))
+									(string->number to-rad))]))
+
+; #|
+; Converts num to a string with radix to-rad
+; |#
+(define (convert-to-string num to-rad)
+	(define (do-thing num to-rad so-far)
+		(cond
+			[(< num to-rad) (string-append (number->string num 16) so-far)]
+			[else (do-thing (floor (/ num to-rad)) to-rad
+							(string-append (number->string (modulo num to-rad) 16) so-far))]))
+	
+	(do-thing num to-rad ""))
