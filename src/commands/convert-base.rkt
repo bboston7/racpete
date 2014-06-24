4f62273fcbb8f31343571d9d93e5fc05d3bd3c3e
Ben Eggers <beggerss@cs.washington.edu>
2014-06-23 23:40:33 -0700

    moved tests to convert-base file

--- src/commands/convert-base.rkt
+++ src/commands/convert-base.rkt
@@ -26,3 +26,41 @@ Converts num to a string with radix to-rad
 							(string-append (number->string (modulo num to-rad) 16) so-far))]))
 	
 	(do-thing num to-rad ""))
+
+
+; Tests
+#|
+Test that we can properly convert to and from various bases
+|#
+(module+ test
+        (require rackunit)
+        (check-equal? "1010" (convert-base "10" "10" "2"))
+        (check-equal? "10" (convert-base "1010" "2" "10"))
+        (check-equal? "2e6" (convert-base "10432" "5" "16"))
+        (check-equal? "10432" (convert-base "2e6" "16" "5"))
+        (check-equal? "11001001000111" (convert-base "52345" "7" "2"))
+        (check-equal? "52345" (convert-base "11001001000111" "2" "7"))
+        (check-equal? "300121021" (convert-base "52345" "14" "4"))
+        (check-equal? "52345" (convert-base "300121021" "4" "14"))
+        (check-equal? "11111" (convert-base "11111" "5" "5"))
+        (check-equal? "2dcea44a2" (convert-base "34657544345" "9" "16"))
+        (check-equal? "34657544345" (convert-base "2dcea44a2" "16" "9"))
+
+        ; Edge cases because if there's a way to break it, max WILL break it
+        (check-equal? "No." (convert-base 5 "9" "4"))
+        (check-equal? "No." (convert-base "5" '() "4"))
+        (check-equal? "No." (convert-base "5" "9" #f))
+        (check-equal? "No." (convert-base 5 9 "4"))
+        (check-equal? "No." (convert-base 5 "9" 4))
+        (check-equal? "No." (convert-base "5" 9 4))
+        (check-equal? "No." (convert-base 5 9 4))
+
+        (check-equal? "No." (convert-base "aoeu" "14" "14"))
+        (check-equal? "No." (convert-base "10" "2f" "14"))
+        (check-equal? "No." (convert-base "500" "14" "n"))
+        (check-equal? "No." (convert-base "5" "4" "14"))
+        (check-equal? "No." (convert-base "344" "2" "14"))
+        (check-equal? "No." (convert-base "100" "17" "14"))
+        (check-equal? "No." (convert-base "100" "14" "17"))
+        (check-equal? "No." (convert-base "100" "1" "14"))
+        (check-equal? "No." (convert-base "100" "14" "1")))
